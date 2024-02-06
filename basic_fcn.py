c20b602e242340bef2114f63cc3cd188b959010d
Jiapei Chen <jic038@ucsd.edu>
2024-02-05 20:30:04 -0800

    Update U-net Architecture and Loss Calculation

--- basic_fcn.py
+++ basic_fcn.py
@@ -21,35 +21,35 @@ class FCN(nn.Module):
         self.relu = nn.ReLU(inplace=True)
         
         # Decoder
-        self.deconv1 = nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
-        self.bn1 = nn.BatchNorm2d(256)
-        self.deconv2 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
-        self.bn2 = nn.BatchNorm2d(128)
-        self.deconv3 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
-        self.bn3 = nn.BatchNorm2d(64)
-        self.deconv4 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
-        self.bn4 = nn.BatchNorm2d(32)
-        self.deconv5 = nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
-        self.bn5 = nn.BatchNorm2d(16)
+        self.deconv1 = nn.ConvTranspose2d(512, 512, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn1 = nn.BatchNorm2d(512)
+        self.deconv2 = nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn2 = nn.BatchNorm2d(256)
+        self.deconv3 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.deconv4 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn4 = nn.BatchNorm2d(64)
+        self.deconv5 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn5 = nn.BatchNorm2d(32)
 
         # Classifier
-        self.classifier = nn.Conv2d(16, self.n_class, kernel_size=1)
+        self.classifier = nn.Conv2d(32, self.n_class, kernel_size=1)
 
     #TODO Complete the forward pass
     def forward(self, x):
         # Encoder
-        x = self.relu(self.bnd1(self.conv1(x)))
-        x = self.relu(self.bnd2(self.conv2(x)))
-        x = self.relu(self.bnd3(self.conv3(x)))
-        x = self.relu(self.bnd4(self.conv4(x)))
-        x5 = self.relu(self.bnd5(self.conv5(x)))
+        x = self.bnd1(self.relu(self.conv1(x)))
+        x = self.bnd2(self.relu(self.conv2(x)))
+        x = self.bnd3(self.relu(self.conv3(x)))
+        x = self.bnd4(self.relu(self.conv4(x)))
+        x5 = self.bnd5(self.relu(self.conv5(x)))
 
         # Decoder
-        x = self.relu(self.bn1(self.deconv1(x5)))
-        x = self.relu(self.bn2(self.deconv2(x)))
-        x = self.relu(self.bn3(self.deconv3(x)))
-        x = self.relu(self.bn4(self.deconv4(x)))
-        x = self.relu(self.bn5(self.deconv5(x)))
+        x = self.bn1(self.relu(self.deconv1(x5)))
+        x = self.bn2(self.relu(self.deconv2(x)))
+        x = self.bn3(self.relu(self.deconv3(x)))
+        x = self.bn4(self.relu(self.deconv4(x)))
+        x = self.bn5(self.relu(self.deconv5(x)))
 
         # Classifier
         score = self.classifier(x)
